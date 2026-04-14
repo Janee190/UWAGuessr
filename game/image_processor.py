c548913d5b15de8a4af3da9af927d1c45fd07a6c
Etienne Unai Vinton Horn <etienneunai@gmail.com>
2026-04-14 23:36:11 +0800

    Switched to webp format to save space

--- game/image_processor.py
+++ game/image_processor.py
@@ -8,7 +8,7 @@ from PIL import Image
 def process_folder(folder_path):
     images = {}
     for filename in os.listdir(folder_path):
-        if filename.lower().endswith((".jpg", ".jpeg", ".png")):
+        if filename.lower().endswith((".jpg", ".jpeg", ".png", ".webp")):
             image_path = os.path.join(folder_path, filename)
             images[filename] = process_image(image_path)
     return images
