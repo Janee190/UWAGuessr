42ef34d7111f9258bea8228c4e8e1873b29a4b7b
fingercodetn <200962471+fingercodingtn@users.noreply.github.com>
2026-05-06 02:26:39 +0800

    added account creation and authentication logic to controllers.py, with supporting helper methods in models to generate and check passwords

--- app/models.py
+++ app/models.py
@@ -1,4 +1,5 @@
 from app import db
+from werkzeug.security import generate_password_hash, check_password_hash
 
 class User(db.Model):
     uid = db.Column(db.Integer, primary_key=True)
@@ -13,3 +14,9 @@ class User(db.Model):
             'username': self.username,
             'email': self.email
         }
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
