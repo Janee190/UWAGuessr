d4b44182b9db65b0664f72b971b44b6236373132
fingercodetn <200962471+fingercodingtn@users.noreply.github.com>
2026-05-06 02:39:04 +0800

    added an initial migration and updated config.py to create app.db at root

--- app/config.py
+++ app/config.py
@@ -1,8 +1,8 @@
 import os
 
-basedir = os.path.abspath(os.path.dirname(__file__))
+basedir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
 default_db_path = 'sqlite:///' + os.path.join(basedir, 'app.db')
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL') or default_db_path
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or default_db_path
     SECRET_KEY = os.environ.get('UWAGUESSR_SECRET_KEY')
