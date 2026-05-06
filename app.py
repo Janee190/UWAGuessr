6a6caee2ea7f2d73463ea8fbfe7f88b6b43961f3
Etienne Unai Vinton Horn <etienneunai@gmail.com>
2026-05-06 14:40:32 +0800

    fixing app.py

--- app.py
+++ app.py
@@ -1,4 +1,27 @@
-from app.routes import app
+from flask import Flask, render_template
+
+application = Flask(__name__)
+
+
+@application.route("/")
+def index():
+    return render_template("index.html")
+
+
+@application.route("/login")
+def login():
+    return render_template("login.html")
+
+
+@application.route("/how-to-play")
+def how_to_play():
+    return render_template("howtoplay.html")
+
+
+@application.route("/leaderboard")
+def leaderboard():
+    return render_template("leaderboard.html")
+
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    application.run(debug=True)
