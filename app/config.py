33e031c3dfbb69bcff556aed5ca593bd63a46d2f
Mathew Tomy <mathewtomy973@gmail.com>
2026-05-10 17:51:01 +0800

    fixed up confic.py merge issues

--- app/config.py
+++ app/config.py
@@ -5,8 +5,4 @@ default_db_path = 'sqlite:///' + os.path.join(basedir, 'app.db')
 
 class Config:
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or default_db_path
-<<<<<<< HEAD
-    SECRET_KEY = os.environ.get('UWAGUESSR_SECRET_KEY') 
-=======
-    SECRET_KEY = os.environ.get('UWAGUESSR_SECRET_KEY') or 'dev-secret-key-change-in-production'
->>>>>>> 41070eb62bd6e7743bbcb5d2c296d25360c13c48
+    SECRET_KEY = os.environ.get('UWAGUESSR_SECRET_KEY')
