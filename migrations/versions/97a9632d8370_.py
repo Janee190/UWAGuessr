593eae3f9ae3705e2908f953558e9a6d1688e669
Etienne Unai Vinton Horn <etienneunai@gmail.com>
2026-05-14 14:34:22 +0800

    merge headers

---
+++ migrations/versions/97a9632d8370_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: 97a9632d8370
+Revises: 03db466e10a7, 87420ac84cf4
+Create Date: 2026-05-14 14:22:43.307621
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '97a9632d8370'
+down_revision = ('03db466e10a7', '87420ac84cf4')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
