2cff2f8a1365996d9f14a3b9baae26bec1aaa3ce
fingercodetn <200962471+fingercodingtn@users.noreply.github.com>
2026-05-17 03:37:27 +0800

    used flask_wtf csrf protection to add protection through csrf tokens to forms in login, signup and forgot_password

--- app/static/js/forgot_password.js
+++ app/static/js/forgot_password.js
@@ -136,10 +136,15 @@ $(function () {
     $btn.addClass("loading").prop("disabled", true);
     $btn.find(".btn-label").text("Resetting…");
 
+    const csrfToken = $("input[name='csrf_token']").val();
+
     $.ajax({
       url: "/api/forgot-password",
       method: "POST",
       contentType: "application/json",
+      headers: {
+        "X-CSRFToken": csrfToken,
+      },
       data: JSON.stringify({
         email: email,
         securityAnswer: securityAnswer,
